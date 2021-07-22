d6594c2bd3a3cac9fbf8878ccf4d2dfa8b599cb8
Timofey Kuzmin <sinnerpocht@gmail.com>
2021-07-22 13:03:25 +0300

    Add message model structure, save new messages into mongo db

---
+++ models.go
@@ -0,0 +1,9 @@
+package main
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Message struct {
+	ID primitive.ObjectID `bson:"_id,omitempty"`
+	Text string	`bson:"text,omitempty"`
+	CreatedAt primitive.DateTime `bson:"created_at"`
+}
--- websockets.go
+++ websockets.go
@@ -1,12 +1,18 @@
-// websockets.go
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/gorilla/websocket"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	//"go.mongodb.org/mongo-driver/mongo/readpref"
+	"net/http"
+	"time"
+	//"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
 )
 
 var upgrader = websocket.Upgrader{
@@ -14,36 +20,91 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-func main() {
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
+const ConnectionUrl string = "mongodb://admin:test@localhost:27017"
 
+func getDbConnect () (*mongo.Database, *mongo.Client, context.Context) {
+	dbClient, err := mongo.NewClient(options.Client().ApplyURI(ConnectionUrl))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = dbClient.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//databases, err := dbClient.ListDatabaseNames(ctx, bson.M{})
+	//if err != nil {
+	//	log.Fatal(err, 1)
+	//}
+	//fmt.Println(databases)
+	//if err := dbClient.Ping(ctx, readpref.Primary()); err != nil {
+	//	log.Fatalf("Connection with db is absent: %s", err)
+	//	return nil
+	//}
+	return dbClient.Database("chat"), dbClient, ctx
+}
+
+
+func main() {
+
+	http.HandleFunc("/messages", func(w http.ResponseWriter, r *http.Request) {
+
+		wsConn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
-			log.Println(err)
+			log.Fatal(err)
 			return
 		}
 
 		for {
 			// Read message from browser
-			msgType, msg, err := conn.ReadMessage()
+			msgType, msg, err := wsConn.ReadMessage()
 			if err != nil {
 				log.Println(err)
 				return
 			}
 
 			// Print the message to the console
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+			fmt.Printf("%s sent: %s\n", wsConn.RemoteAddr(), string(msg))
+
+			db, dbClient, ctx := getDbConnect()
+			defer dbClient.Disconnect(ctx)
+			coll := db.Collection("messages")
+			message :=  Message{
+				Text: string(msg),
+				CreatedAt: primitive.NewDateTimeFromTime(time.Now()),
+			}
+			insertResult, err := coll.InsertOne(ctx, message)
+
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(insertResult.InsertedID)
 
 			// Write message back to browser
-			if err = conn.WriteMessage(msgType, msg); err != nil {
+			if err = wsConn.WriteMessage(msgType, msg); err != nil {
+				log.Println(err)
 				return
 			}
 		}
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		db, dbClient, ctx := getDbConnect()
+		defer dbClient.Disconnect(ctx)
+		msgCollection := db.Collection("messages")
+		var messages []Message
+		cursor, err := msgCollection.Find(ctx, bson.M{})
+		if err != nil {
+			panic(err)
+		}
+		if err = cursor.All(ctx, &messages); err != nil {
+			panic(err)
+		}
+		fmt.Printf("Messages: %s", messages)
 		http.ServeFile(w, r, "websockets.html")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
